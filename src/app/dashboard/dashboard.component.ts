a23590cbdb72971c66f359ba89dabc483fc0d693
ODOH K. Simon <pro.simonkoudjo.9204@gmail.com>
2024-02-26 21:11:17 +0000

    FC

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.scss',
+})
+export class DashboardComponent {
+  status = false;
+  addToggle() {
+    this.status = !this.status;
+  }
+}
